5ce98ad70c0f6789af88c1229708e863ccde3e98
Ruoyu Ying <ruoyu.ying@intel.com>
2024-06-24 15:11:43 +0800

    fix: fix issues

--- tdx/tdx_linux.go
+++ tdx/tdx_linux.go
@@ -99,7 +99,7 @@ func GenerateQuote(tdx device, userData []byte) ([]byte, error) {
 
 	tdreport := cctdx.TdxReport{}
 	var ok bool
-	if tdreport, ok = &report.(cctdx.TdxReport); !ok {
+	if tdreport, ok = *report.(cctdx.TdxReport); !ok {
 		return nil, fmt.Errorf("Failed in fetching TDX Quote.")
 	}
 	fullReport := append(tdreport.Quote.Header.raw.Binary, tdreport.Quote.Body.raw.Binary)
